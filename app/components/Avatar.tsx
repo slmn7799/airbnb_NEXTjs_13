9d25104356c52db64a01c1e0be78a5fc7827c1ff
salman <salmann9977@gmail.com>
2023-04-07 03:04:45 +0530

    adding dropdown in nav bar

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,9 +1,21 @@
+'use client'
 
+import Image from "next/image";
 
-const Avatar = () => {
+interface AvatarProps {
+    src: string | null | undefined;
+}
+
+const Avatar: React.FC<AvatarProps> = ({src}) => {
   return (
     <div>
-      
+      <Image 
+        className="rounded-full" 
+        height="30" 
+        width="30" 
+        alt="Avatar" 
+        src={src || '/images/placeholder.jpg'}
+      />
     </div>
   )
 }
