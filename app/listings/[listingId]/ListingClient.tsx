abecdbbab10a3f92c4f0fef1694771192f1bc821
salman <salmann9977@gmail.com>
2023-04-11 17:50:06 +0530

    creating listing reservation component

---
+++ app/listings/[listingId]/ListingClient.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import axios from "axios";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { toast } from "react-hot-toast";
+import { Range } from "react-date-range";
+import { useRouter } from 'next/navigation';
+import { differenceInDays, eachDayOfInterval } from "date-fns";
+
+import useLoginModal from "@/app/hooks/useLoginModal";
+import { SafeListing, SafeReservation, SafeUser } from "@/app/types";
+
+import Container from "@/app/components/Container";
+import { categories } from "@/app/components/navbar/Categories";
+import ListingHead from "@/app/components/listings/ListingHead";
+import ListingInfo from "@/app/components/listings/ListingInfo";
--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,1 +1,6 @@
 import getCurrentUser from "@/app/actions/getCurrentUser";
+import getListingById from "@/app/actions/getListingById";
+
+import ClientOnly from "@/app/components/ClientOnly";
+import EmptyState from "@/app/components/EmptyState";
+
